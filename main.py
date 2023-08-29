fea9558ed42cb15c6bf4a9bb85c7f4b3a877fc7e
robinvalk09 <94146794+robinvalk09@users.noreply.github.com>
2023-08-29 03:33:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.command()
+async def say(ctx, *, message):
+    await ctx.send(message)
+    await ctx.message.delete()
+
+@bot.command()
+async def quote(ctx):
+    quotes = [
+        "Now I am become Death, the destroyer of worlds.",
+        "The atomic bomb made the prospect of future war unendurable. It has led us up those last few steps to the mountain pass; and beyond there is a different country.",
+        "The optimist thinks this is the best of all possible worlds. The pessimist fears it is true.",
+        # Add more quotes here
+    ]
+    
+    random_quote = random.choice(quotes)
+    await ctx.send(f'"{random_quote}"')
+
+bot.run('MTE0NTQyMDU5OTA4NzM1MzkyOA.Gyj8RH.ROQokQvhDSdojpzVc-2YxHh8JjqQa9TQD216r0')
 
